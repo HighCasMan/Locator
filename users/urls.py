289a777639e5812569b2a6845afeb9dbc435cafe
HighCasMan <borody.rula@gmail.com>
2023-12-21 21:20:31 +0300

    register and auth fix

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from django.views.generic import TemplateView
+
 from users import views
 from users.views import Register, Login, PasswordReset
 
@@ -8,6 +8,4 @@ urlpatterns = [
     path('register/', Register.as_view(), name='register'),
     path("login/", Login.as_view(), name="login"),
     path("password_reset/", PasswordReset.as_view(), name="password_reset"),
-    path('mainpage/', TemplateView.as_view(template_name='main.html'), name='mainpage'),
-    path('search/', TemplateView.as_view(template_name='../templates/search.html'), name='search'),
 ]
