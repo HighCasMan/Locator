dba4f43e61ed47a3936bab7249cb74d81b460054
HighCasMan <borody.rula@gmail.com>
2024-07-21 08:57:40 +0300

    test_selenium.py debug

--- users/tests/test_selenium.py
+++ users/tests/test_selenium.py
@@ -9,33 +9,29 @@ def test_browser():
     return chrome_browser
 
 
-def test_pass():
-    pass
+def test_registration(test_browser):
+    email = "AutoTest@gmail.com"
+    username = "AutoTest"
+    password1 = "AutoTest123."
+    password2 = password1
+
+    register_url = 'http://127.0.0.1:8000/users/register'
+    test_browser.get(register_url)
+
+    email_field = test_browser.find_element(By.ID, value="id_email")
+    username_field = test_browser.find_element(By.ID, value="id_username")
+    password1_field = test_browser.find_element(By.ID, value="id_password1")
+    password2_field = test_browser.find_element(By.ID, value="id_password2")
+
+    email_field.send_keys(email)
+    username_field.send_keys(username)
+    password1_field.send_keys(password1)
+    password2_field.send_keys(password2)
+
+    register_button = test_browser.find_element(By.CLASS_NAME, value="button")
+    register_button.click()
 
 
-# def test_registration(test_browser):
-#     email = "AutoTest@gmail.com"
-#     username = "AutoTest"
-#     password1 = "AutoTest123."
-#     password2 = password1
-#
-#     register_url = 'http://127.0.0.1:8000/users/register'
-#     test_browser.get(register_url)
-#
-#     email_field = test_browser.find_element(By.ID, value="id_email")
-#     username_field = test_browser.find_element(By.ID, value="id_username")
-#     password1_field = test_browser.find_element(By.ID, value="id_password1")
-#     password2_field = test_browser.find_element(By.ID, value="id_password2")
-#
-#     email_field.send_keys(email)
-#     username_field.send_keys(username)
-#     password1_field.send_keys(password1)
-#     password2_field.send_keys(password2)
-#
-#     register_button = test_browser.find_element(By.CLASS_NAME, value="button")
-#     register_button.click()
-#
-#
 # def test_login(test_browser):
 #     email = "AutoTest@gmail.com"
 #     password = "AutoTest123."
